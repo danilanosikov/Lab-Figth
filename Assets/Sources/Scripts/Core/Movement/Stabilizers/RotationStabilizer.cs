1e388766a56066f3bfe1c29c113e3158a709930b
Nosikov Daniil <danilanosikov@gmail.com>
2022-11-04 16:12:38 +0300

    Rotation Stabiliser Addition

--- Assets/Sources/Scripts/Core/Movement/Stabilizers/RotationStabilizer.cs
+++ Assets/Sources/Scripts/Core/Movement/Stabilizers/RotationStabilizer.cs
@@ -1,28 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
+[SelectionBase]
 public class RotationStabilizer : MonoBehaviour
 {
-    // Stiffnes of an imaginary spring attached to the ends of the up vector and the tilt vector
-    [SerializeField] float stiffness = 2f;
-    [SerializeField] float damping = 4;
-
     Rigidbody body;
-    Vector3 pVelocity => body.GetPointVelocity(applicationPoint);
-    Vector3 pAcceleration => pVelocity.magnitude*pVelocity/2;
-    Vector3 applicationPoint => new(transform.position.x, (transform.position.y + 1) * transform.localScale.y, transform.position.z);
-    Vector3 rForce => 1000 * stiffness * tilt.normalized; // Spring resistance force (k * dx = F);
-    Vector3 dForce => damping * pAcceleration; // Damping Force;
 
-    float dt => Time.deltaTime;
-    Vector3 tilt => Vector3.up - transform.up;
-
-    // Start is called before the first frame update
-    void Start() => body = gameObject.GetComponent<Rigidbody>();
-
-    // Update is called once per frame
-    void Update() {
-        body.AddForceAtPosition(dt * (rForce - pAcceleration), applicationPoint); 
+    void Start() {
+        body = gameObject.GetComponent<Rigidbody>();
+        body.centerOfMass = new(body.centerOfMass.x, body.centerOfMass.y - transform.localScale.y, body.centerOfMass.z);
     }
 }
