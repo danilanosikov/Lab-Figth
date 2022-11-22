d8c983e35a668ed3ee89b4f422f5c988c7f74091
Nosikov Daniil <danilanosikov@gmail.com>
2022-11-22 17:45:48 +0300

    Camera and Controls Fix

--- Assets/Entities/Player/Behavior/PlayerController.cs
+++ Assets/Entities/Player/Behavior/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cappa.Core;
@@ -10,14 +11,31 @@ namespace Cappa.Player
     public class PlayerController : MonoBehaviour
     {
 
-        [SerializeField] float limit = 50f, swiftness = 1;
 
         Rigidbody body;
         Vector2 input;
 
 
+
+        [SerializeField] float limit = 50f, swiftness = 5f;
+
+
+
+        public float Speed => Mathf.Abs(body.velocity.magnitude);
+        public float Limit => limit;
+
+
+
+        // Camera
+        Camera camera => transform.parent.Find("Camera").gameObject.GetComponent<Camera>();
+        Vector3 forward =>  new(camera.transform.forward.x, 0, camera.transform.forward.z);
+        Vector3 right => new(camera.transform.right.x, 0, camera.transform.right.z);
+
+
+        // Unity Default
         void Start() => body = gameObject.GetComponent<Rigidbody>();
         void FixedUpdate() => Move();
+
 
 
 
@@ -29,18 +47,32 @@ namespace Cappa.Player
 
 
 
+
+
+
+
+
+
         void Accelerate(float strength = 1) {
 
-            var inp = (Vector3) input; inp.z = inp.y; inp.y = 0;
+            var dir = input.y * forward + input.x * right;
+
             var vel = body.velocity; vel.y = 0;
 
-            var turn =  inp + Mathf.Abs(Vector3.Dot(vel, inp)) * inp.normalized;
+            var turn =  dir + Mathf.Abs(Vector3.Dot(vel, dir)) * dir.normalized;
 
-            var frce = inp + turn;
+            var frce = dir + turn;
             frce.y = 0;
 
             Move(strength * frce);
         }
+
+
+
+
+
+
+
 
 
 
@@ -54,10 +86,24 @@ namespace Cappa.Player
 
 
 
+
+
+
+
+
+
+
         void Move(Vector3 dir) {
             body.AddForce(swiftness * body.mass * dir);
             if (body.velocity.magnitude > limit) body.velocity = body.velocity / body.velocity.magnitude * limit;
         }
+
+
+
+
+
+
+
 
         void OnMove(InputValue inp) => input = inp.Get<Vector2>();
     }
